5c3556c5ac2a177dacdd7731ba4f21ef901463a4
pigamers <pigamers1123@gmail.com>
2024-07-30 15:11:53 +0530

    error page minor change

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -21,12 +21,6 @@ export default function ErrorPage() {
                         <MdArrowBack size={25} />
                         Go back
                     </Link>
-                    <button
-                        type="button"
-                        className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                    >
-                        Contact us
-                    </button>
                 </div>
             </div>
         </div>
